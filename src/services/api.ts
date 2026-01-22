96057fb150e578c01c7a2e45e30090041dd0b7eb
Anthony Sung <sungpinyue@gmail.com>
2026-01-22 14:52:13 +0800

    first commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosError, AxiosResponse } from 'axios';
 
 const api: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/',
     headers: {
         'Content-Type': 'application/json',
     },
